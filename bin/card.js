206c5ee3a0bec7dd54ea82f4679dfd8a727e9f8f
Stephen Coogan <coogie@users.noreply.github.com>
2023-04-16 03:02:42 +0100

    chore: switch to prettier

--- bin/card.js
+++ bin/card.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-'use strict'
+"use strict";
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 import fs from "fs";
 import path, { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
+const output = fs.readFileSync(path.join(__dirname, "output"), "utf8");
+console.log(output);
